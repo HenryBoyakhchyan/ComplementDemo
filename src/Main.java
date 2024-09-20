4351352bf0a76e44d26ef6c887375048ab0ebbab
HenryBoyakhchyan <henry.boyakhchyan@gmail.com>
2024-09-20 21:05:05 +0400

    init

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        int a = 5;
+        int b = -5;
+        System.out.println("the sum is " + add(a,b));
+        System.out.println("Стол");
+    }
+
+    static int add(int a, int b){
+        return a + b;
+    }
+}
